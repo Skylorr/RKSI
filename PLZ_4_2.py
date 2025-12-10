2b20e8f02027711c1df22bc620318bc6476046d6
Skylor <106466061+Skylorr@users.noreply.github.com>
2025-12-10 17:17:09 +0300

    Create PLZ_4_2.py

---
+++ PLZ_4_2.py
@@ -0,0 +1,15 @@
+n = input("Введите число N (>0): ")
+
+while type(n) != int:  # обработка исключений
+    try:
+        n = int(n)
+    except ValueError:
+        print("Неправильно ввели!")
+        n = input("Введите число N (>0): ")
+
+rev = 0
+while n > 0:
+    rev = rev * 10 + (n % 10)
+    n //= 10
+
+print("Перевернутое число:", rev)
