a551eaba066d673c2e34a056c95f6fba0c9b603d
Skylor <106466061+Skylorr@users.noreply.github.com>
2025-12-10 17:07:44 +0300

    Create PLZ_2_1.py

---
+++ PLZ_2_1.py
@@ -0,0 +1,20 @@
+a, b = input("Введите A: "), input("Введите B: ")
+
+while type(a) != int:
+    try:
+        a = int(a)
+    except ValueError:
+        print("Неправильно ввели!")
+        a = input("Введите A: ")
+
+while type(b) != int:
+    try:
+        b = int(b)
+    except ValueError:
+        print("Неправильно ввели!")
+        b = input("Введите B: ")
+
+if a > 2 and b < 3:
+    print("Высказывание истинно.")
+else:
+    print("Высказывание ложно.")
