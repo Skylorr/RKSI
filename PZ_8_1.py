cec81daa750da080b9f8a484424bf3abe52269a9
Skylor <106466061+Skylorr@users.noreply.github.com>
2026-01-24 16:21:00 +0300

    Update PZ_8_1.py

--- PZ_8_1.py
+++ PZ_8_1.py
@@ -2,7 +2,6 @@
 
 
 sample_dict = {'Physics': 82, 'Math': 65, 'history': 75}
-
 min_key = min(sample_dict, key=sample_dict.get)
 print('Ключ с минимальным значением:', min_key)
 print('Минимальное значение:', sample_dict[min_key])
