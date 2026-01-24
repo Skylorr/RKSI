b01d301056f988ee4c8313f27a6571d5635f1b7a
Skylor <106466061+Skylorr@users.noreply.github.com>
2026-01-24 16:20:01 +0300

    Update PZ_8_1.py

--- PZ_8_1.py
+++ PZ_8_1.py
@@ -1,1 +1,8 @@
+#Найдите ключ с минимальным значением в sample_dict = {'Physics': 82, 'Math': 65,'history': 75}
 
+
+sample_dict = {'Physics': 82, 'Math': 65, 'history': 75}
+
+min_key = min(sample_dict, key=sample_dict.get)
+print('Ключ с минимальным значением:', min_key)
+print('Минимальное значение:', sample_dict[min_key])
