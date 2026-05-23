112f6793b401b758d4c2cff04e4196cd8bee89e6
Skylor <106466061+Skylorr@users.noreply.github.com>
2026-05-23 17:06:00 +0300

    Update PZ_12_1.py

--- Manakova/second_semester/PZ_12_1.py
+++ Manakova/second_semester/PZ_12_1.py
@@ -1,28 +1,18 @@
-# В квадратной матрице все элементы, не лежащие на главной диагонали увеличить в 2 раза.
+# В квадратной матрице все элементы, не лежащие на главной диагонали увеличить в
+# 2 раза.
 
-import random
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
+print("Исходная квадратная матрица:")
+for row in matrix:
+    print(row)
 
-def process_matrix_diagonal():
-    size = 4
-    matrix = [[random.randint(1, 9) for _ in range(size)] for _ in range(size)]
+size = len(matrix)
+updated_matrix = [
+    [matrix[i][j] if i == j else matrix[i][j] * 2 for j in range(size)]
+    for i in range(size)
+]
 
-    print("Исходная квадратная matrix:")
-    for row in matrix:
-        print(row)
-
-    updated_matrix = [
-        [
-            matrix[i][j] if i == j else matrix[i][j] * 2
-            for j in range(size)
-        ]
-        for i in range(size)
-    ]
-
-    print("\nРезультирующая matrix:")
-    for row in updated_matrix:
-        print(row)
-
-
-if __name__ == "__main__":
-    process_matrix_diagonal()
+print("\nРезультирующая матрица:")
+for row in updated_matrix:
+    print(row)
