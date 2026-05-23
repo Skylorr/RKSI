7476701d64bcb31df486cb4c69db4969b81a8475
Skylor <106466061+Skylorr@users.noreply.github.com>
2026-05-23 17:00:41 +0300

    Fix string quotes and update comments in PZ_8_1.py

--- Manakova/second_semester/PZ_8_1.py
+++ Manakova/second_semester/PZ_8_1.py
@@ -1,6 +1,8 @@
-#Найдите ключ с минимальным значением в sample_dict = {'Physics': 82, 'Math': 65,'history': 75}
+# Найдите ключ с минимальным значением в sample_dict = {'Physics': 82, 'Math': 65,
+# 'history': 75}
 
-sample_dict = {'Physics': 82,'Math': 65,'history': 75}
+sample_dict = {"Physics": 82, "Math": 65, "history": 75}
+print(f"Исходный словарь: {sample_dict}")
 
 min_key = min(sample_dict, key=sample_dict.get)
 print(f"Ключ с минимальным значением: {min_key}")
