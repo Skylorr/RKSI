3302fb2310cec6dcb7be655d1b3c1aa967e851ff
Skylor <106466061+Skylorr@users.noreply.github.com>
2026-05-23 17:10:27 +0300

    Update PZ_11_2.py

--- Manakova/second_semester/PZ_11_2.py
+++ Manakova/second_semester/PZ_11_2.py
@@ -6,14 +6,9 @@ def letter_generator(input_string):
             yield char
 
 
-def run_generator_task():
-    source_text = "PyCharm Community 2026! Лабораторная работа №11."
-    print(f"Исходная строка: '{source_text}'")
+source_text = "PyCharm Community 2026! Лабораторная работа №11."
+print(f"Исходная строка: '{source_text}'")
 
-    gen = letter_generator(source_text)
-    result_text = "".join(gen)
-    print(f"Результат работы генератора: '{result_text}'")
-
-
-if __name__ == "__main__":
-    run_generator_task()
+gen = letter_generator(source_text)
+result_text = "".join(gen)
+print(f"Результат работы генератора: '{result_text}'")
