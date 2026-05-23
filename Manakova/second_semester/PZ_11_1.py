3b20afdb11d6a52021d74f0f9a55c55f9a2c5797
Skylor <106466061+Skylorr@users.noreply.github.com>
2026-05-23 16:20:15 +0300

    Update PZ_11_1.py

--- Manakova/second_semester/PZ_11_1.py
+++ Manakova/second_semester/PZ_11_1.py
@@ -2,20 +2,18 @@
 
 import random
 
-def process_sequence():
-    print("--- Задача 1: Умножение элементов на последний элемент ---")
 
+def process_sequence():
     n = 7
     initial_sequence = [random.randint(1, 10) for _ in range(n)]
-    print(f"Исходная последовательность (n = {n}): {initial_sequence}")
+    print(f"Исходная последовательность: {initial_sequence}")
 
     last_element = initial_sequence[-1]
-    print(f"Последний элемент (n): {last_element}")
+    print(f"Последний элемент: {last_element}")
 
     result_sequence = [
         x * last_element for x in initial_sequence[:-1]
     ] + [last_element]
-
     print(f"Результирующая последовательность: {result_sequence}\n")
 
 
