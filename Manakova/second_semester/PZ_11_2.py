35a4deab581eca02ba33929036fc23a1aa40e27d
Skylor <106466061+Skylorr@users.noreply.github.com>
2026-05-23 16:20:33 +0300

    Update PZ_11_2.py

--- Manakova/second_semester/PZ_11_2.py
+++ Manakova/second_semester/PZ_11_2.py
@@ -5,16 +5,15 @@ def letter_generator(input_string):
         if char.isalpha():
             yield char
 
-def run_generator_task():
-    print("--- Задача 2: Генератор букв из строки ---")
 
-    source_text = "Python 3.10 - это отличный язык! Год 2026."
+def run_generator_task():
+    source_text = "PyCharm Community 2026! Лабораторная работа №11."
     print(f"Исходная строка: '{source_text}'")
 
     gen = letter_generator(source_text)
+    result_text = "".join(gen)
+    print(f"Результат работы генератора: '{result_text}'")
 
-    result_letters = "".join(gen)
-    print(f"Результат работы генератора (только буквы): '{result_letters}'")
 
 if __name__ == "__main__":
     run_generator_task()
