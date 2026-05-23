71a0b90347085808cf8576c8ccb7bce1cb196bb8
Skylor <106466061+Skylorr@users.noreply.github.com>
2026-05-23 17:10:13 +0300

    Update PZ_11_1.py

--- Manakova/second_semester/PZ_11_1.py
+++ Manakova/second_semester/PZ_11_1.py
@@ -1,21 +1,12 @@
 # В последовательности на n целых чисел умножить элементы до n-1 на элемент n.
 
-import random
+initial_sequence = [12, 3.85, 5, -4, 7]
+print(f"Исходная последовательность: {initial_sequence}")
 
+last_element = initial_sequence[-1]
+print(f"Последний элемент: {last_element}")
 
-def process_sequence():
-    n = 7
-    initial_sequence = [random.randint(1, 10) for _ in range(n)]
-    print(f"Исходная последовательность: {initial_sequence}")
-
-    last_element = initial_sequence[-1]
-    print(f"Последний элемент: {last_element}")
-
-    result_sequence = [
-        x * last_element for x in initial_sequence[:-1]
-    ] + [last_element]
-    print(f"Результирующая последовательность: {result_sequence}\n")
-
-
-if __name__ == "__main__":
-    process_sequence()
+result_sequence = [x * last_element for x in initial_sequence[:-1]] + [
+    last_element
+]
+print(f"Результирующая последовательность: {result_sequence}")
