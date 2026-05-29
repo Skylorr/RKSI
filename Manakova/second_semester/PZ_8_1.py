7b1f0b40dff688d5cb82a87ca796e5e52b694818
Skylor <106466061+Skylorr@users.noreply.github.com>
2026-05-29 17:06:26 +0300

    Update PZ_8_1.py

--- Manakova/second_semester/PZ_8_1.py
+++ Manakova/second_semester/PZ_8_1.py
@@ -2,7 +2,5 @@
 # 'history': 75}
 
 sample_dict = {"Physics": 82, "Math": 65, "history": 75}
-print(f"Исходный словарь: {sample_dict}")
-
 min_key = min(sample_dict, key=sample_dict.get)
 print(f"Ключ с минимальным значением: {min_key}")
